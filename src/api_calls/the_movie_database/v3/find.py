4840ea3710608d43f25232adbe384d76cdb066b1
Tom Adriaensen <tom@adriaensen.eu>
2022-12-19 20:36:03 +0100

    python update

--- src/api_calls/the_movie_database/v3/find.py
+++
@@ -1,9 +0,0 @@
-from api_calls.api_call import ApiCall
-
-
-class TmdbCertifications(ApiCall):
-    def __init__(self, api_name: str):
-        super().__init__(api_name=api_name)
-
-    def find_by_id(self) -> dict:
-        pass
